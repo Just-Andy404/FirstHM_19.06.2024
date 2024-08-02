9c938385f9939dc9e70dff04bf8280236909755e
Andy <andyivanov2007@gmail.com>
2024-08-02 20:46:19 +0300

    Add project files.

---
+++ ConsoleApp1/Film.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public string Studio { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public int Year { get; set; }
+
+        public Film(string name, string studio, string genre, int duration, int year)
+        {
+            Name = name;
+            Studio = studio;
+            Genre = genre;
+            Duration = duration;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Киностудия: {Studio}");
+            Console.WriteLine($"Жанр: {Genre}");
+            Console.WriteLine($"Длительность: {Duration}");
+            Console.WriteLine($"Год: {Year}");
+            return "";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Фильм удален c помощью Dispose");
+        }
+
+        ~Film()
+        {
+            Console.WriteLine("Фильм удален");
+        }
+
+    }
+}
